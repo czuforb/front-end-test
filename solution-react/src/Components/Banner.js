0c199cb5f6193182a58ef024baa597a1578e9c7c
Bence CZufor <czuforbence@gmail.com>
2020-09-22 17:45:51 +0200

    style: banner styling -wip

--- solution-react/src/Components/Banner.js
+++ solution-react/src/Components/Banner.js
@@ -5,23 +5,25 @@ import './Banner.css';
 
 const Banner = () => {
     return (
-        <section>
-            <div>
+        <section className="banner">
+            <div className="banner__box-left">
                 <h3 className="banner__author">Pil Damgaard
-                    <span className="banner_year"> 2018</span>
+                    <span className="banner__year"> 2018</span>
                 </h3>
                 <p>This section is actually a little extra that can be added if you feel confident while working and the time aint sprinting.</p>
                 <a href="#">
                     <Arrow />
                 </a>
             </div>
-            <div>
-            <h2 className="banner__subtitle">#extra</h2>
-            <h2 className="banner__title">Red arrows</h2>
-            <h2 className="banner__author">Pil Damgaard</h2>
-            <a href="">
-                <ArrowLeft/>
-            </a>
+            <div className="banner__box-right">
+                <h2 className="banner__subtitle">#extra</h2>
+                <h2 className="banner__title">
+                    Pil damgaard<br/>
+                    <span>Red arrows</span>
+                </h2>
+                    <a href="">
+                        <ArrowLeft/>
+                    </a>
             </div>
             
         </section>
